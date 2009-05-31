2fc95b1e89f3f96bc449feff4889362d8ea9c85a
cmsbuild <>
2009-05-31 12:51:22 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- CastorRawToDigi-V00-00-01-00'.
    Sprout from master 2008-06-20 11:22:13 UTC Alan James Campbell <sha1-de72a6f3f1883deb24a3b110a8f1931c08214c67@cern.ch> 'initial'
    Cherrypick from master 2009-05-31 12:51:21 UTC Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch> 'update to new path for Crc.h to allow build of 2.2.12_HLT':
        src/CastorPacker.cc

--- src/CastorPacker.cc
+++ src/CastorPacker.cc
@@ -4,7 +4,7 @@
 #include "DataFormats/HcalDetId/interface/HcalGenericDetId.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/FEDRawData/interface/FEDTrailer.h"
-#include "EventFilter/Utilities/interface/Crc.h"
+#include "FWCore/Utilities/interface/CRC16.h"
 
 CastorPacker::Collections::Collections() {
   castorCont=0;
